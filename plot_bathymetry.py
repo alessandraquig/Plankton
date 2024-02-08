2a1562fd62528d8782dd3b76a5fe2757fc3facb9
alessandraquig <77391150+alessandraquig@users.noreply.github.com>
2024-02-08 00:52:01 +0000

    plotting bathymetry

---
+++ plot_bathymetry.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import netCDF4 as nc
+import os
+import numpy as np
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+
+# Read the netCDF file
+path = "Data/bathymetry.nc"
+data = nc.Dataset(path)
+var_name = 'elevation'
+
+# Extract the latitude and longitude variables
+lat_var = data.variables.get('lat') or data.variables.get('latitude')
+if lat_var is not None:
+    lat = lat_var[:]
+else:
+    raise ValueError("Latitude variable not found in the netCDF file.")
+lon_var = data.variables.get('lon') or data.variables.get('longitude')
+if lon_var is not None:
+    lon = lon_var[:]
+else:
+    raise ValueError("Longitude variable not found in the netCDF file.")
+
+# Extract the data variable you want to plot
+var = data.variables[var_name][:]
+print(var.min(), var.max())
+
+# Create a figure and axes with a specific projection
+fig, ax = plt.subplots(figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree()})
+
+# Creating levels
+#norm = BoundaryNorm(levels=np.linspace(), ncolors=cmap.N, clip=True)
+
+# Plot the data on a latitude and longitude scale
+#im = ax.pcolormesh(lon, lat, var, transform=ccrs.PlateCarree(),  cmap='coolwarm')
+
+# Plot the data on a latitude and longitude scale
+im = ax.contourf(lon, lat, var, transform=ccrs.PlateCarree(), cmap='autumn')
+
+# Set the extent of the map to match your data
+ax.set_extent([-180, -65, -70, 0], crs=ccrs.PlateCarree())
+
+# Add parallels and meridians
+ax.gridlines(draw_labels=False, linewidth=0.5, color='grey', alpha=0.5, linestyle='-')
+ax.set_xticks(np.arange(-180, -55, 10), crs=ccrs.PlateCarree())
+ax.set_yticks(np.arange(-70, 10, 10), crs=ccrs.PlateCarree())
+ax.xaxis.set_major_formatter(LongitudeFormatter())
+ax.yaxis.set_major_formatter(LatitudeFormatter())
+
+# Mask out land
+ax.add_feature(cfeature.LAND, zorder=1, facecolor='w')
+
+# Add coastlines
+ax.coastlines('50m')
+#ax.add_feature(cfeature.COASTLINE, zorder=2)
+
+# Add a colorbar
+cbar = plt.colorbar(im, ax=ax)
+
+# Set the title and labels
+ax.set_title('Depth of the Ocean Floor')
+ax.set_xlabel('Longitude')
+ax.set_ylabel('Latitude')
+
+# Show the plot
+plt.show()
+
+# Save the plot as a tif file
+#plt.savefig('Output/bathymetry.tif', format='tif')
+
+# Close the plot
+#plt.close()
