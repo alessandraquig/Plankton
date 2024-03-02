56969ef54d6471b27b79d9cd8cd3cd95c7bfaaee
alessandraquig <77391150+alessandraquig@users.noreply.github.com>
2024-03-02 12:53:37 +0300

    adjusting color scales

--- plot_chlorophyll.py
+++ plot_chlorophyll.py
@@ -1,11 +1,11 @@
 import matplotlib
-from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import netCDF4 as nc
 import numpy as np
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+from matplotlib.colors import LinearSegmentedColormap
 
 matplotlib.use('qtagg')
 
@@ -32,19 +32,18 @@ lon = lon_var[:]
 var = data.variables[var_name][:]
 print(f'min = {var.min()}, max = {var.max()}, mean = {var.mean()}')
 
+# Define custom levels for contourf
+levels = [0, 0.1, 0.3, 0.6, 1, 2, 5]
+
+# Create a custom colormap emphasizing low values
+colors = ['#f7fcf5', '#a1d99b', '#74c476', '#41ab5d', '#238b45', '#127031', '#00441b']
+cmap = LinearSegmentedColormap.from_list("custom_green", colors)
+
 # Create a figure and axes with a specific projection
 fig, ax = plt.subplots(figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree()})
 
-# Creating levels
-#norm = BoundaryNorm(levels=np.linspace(), ncolors=cmap.N, clip=True)
-
-# Plot the data on a latitude and longitude scale
-#im = ax.pcolormesh(lon, lat, var, transform=ccrs.PlateCarree(),  cmap='coolwarm')
-
-# Plot the data on a latitude and longitude scale
-colors = ['#f8fcf6','#e2f1da', '#bfdfb3', '#8fc483', '#74b270', '#48854a', '#1f4521']
-cmap = ListedColormap(colors)
-im = ax.contourf(lon, lat, var, transform=ccrs.PlateCarree(), cmap=cmap, levels = [0, 0.1, 0.3, 0.6, 1, 2, 5])
+# Plot the data on a latitude and longitude scale using the custom colormap
+im = ax.contourf(lon, lat, var, transform=ccrs.PlateCarree(), cmap=cmap, levels=levels) #, extend='max')
 
 # Set the extent of the map to match your data
 ax.set_extent([-180, -65, -70, 0], crs=ccrs.PlateCarree())
@@ -61,7 +60,6 @@ ax.add_feature(cfeature.LAND, zorder=1, facecolor='w')
 
 # Add coastlines
 ax.coastlines('50m')
-#ax.add_feature(cfeature.COASTLINE, zorder=2)
 
 # Add a colorbar
 cbar = plt.colorbar(im, ax=ax)
@@ -74,11 +72,8 @@ ax.set_ylabel(r'Latitude')
 # Show the plot
 plt.show()
 
-# Save plot as png
-#plt.savefig('Output/chlorophyll_mean.png', format='png')
-
 # Save the plot as a tif file
-#plt.savefig('Output/chlorophyll_mean.tif', format='tif')
+plt.savefig('Output/chlorophyll_mean.tif', format='tif')
 
 # Close the plot
 plt.close()
