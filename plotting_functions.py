bd7e9acb2488ae3009660d3b187f2ccbf4f56946
alessandraquig <77391150+alessandraquig@users.noreply.github.com>
2024-03-13 21:05:58 +0200

    SDM combined plots

--- plotting_functions.py
+++ plotting_functions.py
@@ -688,3 +688,142 @@ def SDM_spatial(plankton, layer, fig, rows, cols, pos=1):
 
     return im, ax, cbar
 
+def SDM_prediction(plankton, layer, fig, rows, cols, pos=1):
+    """
+    :param plankton: "phyto" or "zoo"
+    :param layer: "surf" or "depth"
+    :return: fig, ax, cbar
+    """
+    path = f"Data/prediction{plankton}{layer}.nc"
+
+    data = nc.Dataset(path)
+    var_name = os.path.basename(path).split(".")[0] # Check this
+    if plankton =='zoo':
+        var_name = 'prediction'
+    elif var_name == 'predictionphytodepth':
+        var_name = 'predictionphyto'
+
+    # Extract the latitude and longitude variables
+    lat_var = data.variables.get('lat') or data.variables.get('latitude')
+    if lat_var is not None:
+        lat = lat_var[:]
+    else:
+        raise ValueError("Latitude variable not found in the netCDF file.")
+    lon_var = data.variables.get('lon') or data.variables.get('longitude')
+    if lon_var is not None:
+        lon = lon_var[:]
+    else:
+        raise ValueError("Longitude variable not found in the netCDF file.")
+
+    # Extract the data variable you want to plot
+    var = data.variables[var_name][:]
+    var[var <= 0] = 1e-2
+    print(var.min(), var.max())
+
+
+    # Create a figure and axes with a specific projection
+    #fig, ax = plt.subplots(figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree()})
+    ax: plt.Axes = fig.add_subplot(rows, cols, pos, projection=ccrs.PlateCarree())
+
+    # Plot the data on a latitude and longitude scale
+    im = ax.contourf(lon, lat, var, transform=ccrs.PlateCarree(), cmap='magma')
+
+    # Set the extent of the map to match your data
+    ax.set_extent([-160, -70, -60, 0], crs=ccrs.PlateCarree())
+
+    # Add parallels and meridians
+    ax.gridlines(draw_labels=False, linewidth=0.5, color='grey', alpha=0.5, linestyle='-')
+    ax.set_xticks(np.arange(-160, -60, 20), crs=ccrs.PlateCarree())
+    ax.set_yticks(np.arange(-60, 10, 10), crs=ccrs.PlateCarree())
+    ax.xaxis.set_major_formatter(LongitudeFormatter())
+    ax.yaxis.set_major_formatter(LatitudeFormatter())
+
+    # Mask out land
+    ax.add_feature(cfeature.LAND, zorder=1, facecolor='w')
+
+    # Add coastlines
+    ax.coastlines('50m')
+    #ax.add_feature(cfeature.COASTLINE, zorder=2)
+
+    # Add a colorbar
+    cbar = plt.colorbar(im, ax=ax)
+    #cbar.ax.yaxis.set_major_locator(plt.LogLocator())
+
+    # Set the title and labels
+    if layer == "surf":
+        layer_name = "Epipelagic"
+    if layer == "depth":
+        layer_name = "Mesopelagic"
+    ax.set_title(rf'Richness')
+    #ax.set_xlabel(r'Longitude')
+    #ax.set_ylabel(r'Latitude')
+
+    return im, ax, cbar
+    """
+    :param plankton: "phyto" or "zoo"
+    :param layer: "surf" or "depth"
+    :return: fig, ax, cbar
+    """
+    path = f"Data/prediction{plankton}{layer}.nc"
+
+    data = nc.Dataset(path)
+    var_name = os.path.basename(path).split(".")[0] # Check this
+    if var_name != 'predictionphytosurf':
+        var_name = 'prediction'
+
+    # Extract the latitude and longitude variables
+    lat_var = data.variables.get('lat') or data.variables.get('latitude')
+    if lat_var is not None:
+        lat = lat_var[:]
+    else:
+        raise ValueError("Latitude variable not found in the netCDF file.")
+    lon_var = data.variables.get('lon') or data.variables.get('longitude')
+    if lon_var is not None:
+        lon = lon_var[:]
+    else:
+        raise ValueError("Longitude variable not found in the netCDF file.")
+
+    # Extract the data variable you want to plot
+    var = data.variables[var_name][:]
+    var[var <= 0] = 1e-2
+    print(var.min(), var.max())
+
+
+    # Create a figure and axes with a specific projection
+    #fig, ax = plt.subplots(figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree()})
+    ax: plt.Axes = fig.add_subplot(rows, cols, pos, projection=ccrs.PlateCarree())
+
+    # Plot the data on a latitude and longitude scale
+    im = ax.contourf(lon, lat, var, transform=ccrs.PlateCarree(), cmap='magma')
+
+    # Set the extent of the map to match your data
+    ax.set_extent([-160, -70, -60, 0], crs=ccrs.PlateCarree())
+
+    # Add parallels and meridians
+    ax.gridlines(draw_labels=False, linewidth=0.5, color='grey', alpha=0.5, linestyle='-')
+    ax.set_xticks(np.arange(-160, -60, 20), crs=ccrs.PlateCarree())
+    ax.set_yticks(np.arange(-60, 10, 10), crs=ccrs.PlateCarree())
+    ax.xaxis.set_major_formatter(LongitudeFormatter())
+    ax.yaxis.set_major_formatter(LatitudeFormatter())
+
+    # Mask out land
+    ax.add_feature(cfeature.LAND, zorder=1, facecolor='w')
+
+    # Add coastlines
+    ax.coastlines('50m')
+    #ax.add_feature(cfeature.COASTLINE, zorder=2)
+
+    # Add a colorbar
+    cbar = plt.colorbar(im, ax=ax)
+    cbar.ax.yaxis.set_major_locator(plt.LogLocator())
+
+    # Set the title and labels
+    if layer == "surf":
+        layer_name = "Epipelagic"
+    if layer == "depth":
+        layer_name = "Mesopelagic"
+    ax.set_title(rf'Richness')
+    #ax.set_xlabel(r'Longitude')
+    #ax.set_ylabel(r'Latitude')
+
+    return im, ax, cbar
